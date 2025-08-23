87c8d9f679a242d034efbd3af457e38042b5d70d
islam <islamatef59@gmail.com>
2025-08-23 19:16:01 +0300

    add query

---
+++ newislam.sql
@@ -0,0 +1,6 @@
+SELECT weather_conditions,
+        AVG (CAST(steps AS BIGINT)) AS avg_steps,
+	   AVG (CAST (calories_burned AS BIGINT )) AS calories_burned ,
+	   AVG(CAST(active_minutes AS BIGINT)) AS active_minutes 
+	   FROM fitness_tracker_dataset
+	   GROUP BY weather_conditions
